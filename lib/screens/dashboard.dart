1ae80b0248db44ece669f611a5bbbc2d1549d182
shishupaltech <veerutech75@gmail.com>
2025-05-30 16:24:50 +0530

    convert dashboard statelesswidget to statefullwidget

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
   logOut() async{
     await FirebaseAuth.instance.signOut();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
