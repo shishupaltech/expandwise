dbb4f259fdf7871f704482bf3cb5617904420954
shishupaltech <veerutech75@gmail.com>
2025-05-30 16:23:40 +0530

    dashboard is created with logout functionality

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
+  logOut() async{
+    await FirebaseAuth.instance.signOut();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.blueAccent,
+        actions: [
+          IconButton(onPressed: (){
+            logOut();
+          }, icon: Icon(Icons.exit_to_app)),
+        ],
+      ),
+      body: Text('Hello'),
+    );
   }
 }
