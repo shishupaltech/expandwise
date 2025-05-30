08b0446da222c19d81299140c76a238646763d65
shishupaltech <veerutech75@gmail.com>
2025-05-30 16:14:24 +0530

    auth_gate is created in widgets folder and dashboard file in screens

---
+++ lib/widgets/auth_gate.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:spendwise/screens/dashboard.dart';
+import 'package:spendwise/screens/login_screen.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if(!snapshot.hasData){
+          return LoginScreen();
+        }
+        return Dashboard();
+      }
+    );
+  }
+}
