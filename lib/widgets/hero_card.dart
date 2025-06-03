f8f17ef8b4a7694d9e1efbe73146d74eee1c3e7a
shishupaltech <veerutech75@gmail.com>
2025-06-03 14:19:04 +0530

    step 17: hero_card widget is created and Herocard code copy and paste here

---
+++ lib/widgets/hero_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class HeroCard extends StatelessWidget {
+  const HeroCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade900,
+      
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    height: 1.2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '₹ 5800988',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                    height: 1.2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+      
+          Container(
+            padding: EdgeInsets.only(top: 30, bottom: 10, left: 10, right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              color: Colors.white,
+            ),
+            child: Row(
+              children: [
+                CardOne(color: Colors.green),
+                SizedBox(width: 10),
+                CardOne(color: Colors.red),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardOne extends StatelessWidget {
+  const CardOne({super.key, required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Credit', style: TextStyle(color: color, fontSize: 30,)),
+                  Text('₹58000', style: TextStyle(color: color, fontSize: 30,fontWeight: FontWeight.w600)),
+                ],
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.arrow_upward_outlined,color: color,)
+              ),
+            ],
+             
+          ),
+        ),
+      ),
+    );
+  }
+}
