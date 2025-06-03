907f36ad478cc93d36b9e9bab352bf83ca2e9986
shishupaltech <veerutech75@gmail.com>
2025-06-03 12:02:28 +0530

    step 12:CardOne is create in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:spendwise/screens/login_screen.dart';
 
+
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,17 +12,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-    var isLogoutLoading = false;
-  logOut() async{
+  var isLogoutLoading = false;
+  logOut() async {
     setState(() {
-      isLogoutLoading=true;
+      isLogoutLoading = true;
     });
     await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoginScreen()),
-      );
+    Navigator.of(
+      context,
+    ).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
     setState(() {
-      isLogoutLoading=false;
+      isLogoutLoading = false;
     });
   }
 
@@ -28,15 +30,91 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Colors.blueAccent,
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Hello, ', style: TextStyle(color: Colors.white)),
         actions: [
-          IconButton(onPressed: (){
-            logOut();
-          }, icon:isLogoutLoading?CircularProgressIndicator():
-           Icon(Icons.exit_to_app)),
+          IconButton(
+            onPressed: () {
+              logOut();
+            },
+            icon:
+                isLogoutLoading
+                    ? CircularProgressIndicator()
+                    : Icon(Icons.exit_to_app, color: Colors.white),
+          ),
         ],
       ),
-      body: Text('Home Screen Page'),
+      body: Container(
+        color: Colors.blue.shade900,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    height: 1.2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '₹ 5800988',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                    height: 1.2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  CardOne(color: 
+                    Colors.green
+                    ),
+                  SizedBox(width: 10,),
+                  CardOne(color: Colors.red,)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardOne extends StatelessWidget {
+  const CardOne({
+    super.key,required this.color
+  });
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color:color.withOpacity(0.1),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Text('Credit'),
+                Text('₹58000')
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
