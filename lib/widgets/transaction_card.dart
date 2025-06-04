c5fe460b9718c016667d231fe9fec31e3354d7b8
shishupaltech <veerutech75@gmail.com>
2025-06-04 13:41:29 +0530

    step 30: Transaction_card is separataly handled in another widget in transanction_card

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -1,26 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:spendwise/utils/icons_list.dart';
 
 class TransactionCard extends StatelessWidget {
   const TransactionCard({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-    
     return Padding(
-      padding:const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(15),
       child: Column(
         children: [
           Row(
             children: [
-              Text('Recent Transactions',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
+              Text(
+                'Recent Transactions',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
               ),
-              
-              )
             ],
           ),
           RecentTransanctionList(),
@@ -31,30 +29,40 @@ class TransactionCard extends StatelessWidget {
 }
 
 class RecentTransanctionList extends StatelessWidget {
-  const RecentTransanctionList({
-    super.key,
-  });
+   RecentTransanctionList({super.key});
+
+  final userId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   Widget build(BuildContext context) {
-    
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: 2,
-      physics: NeverScrollableScrollPhysics(),
-    
-      itemBuilder: (context, index){
-      
-      return TransanctionCard();
-    });
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').doc(userId).collection('transactions').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }else if(snapshot.connectionState== ConnectionState.waiting){
+          return Text('Loading');
+        }else if(!snapshot.hasData || snapshot.data!.docs.isEmpty){
+          return const Center(child: Text('No Transanctions found.'));
+        }
+        var data = snapshot.data!.docs;
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: 2,
+          physics: NeverScrollableScrollPhysics(),
+
+          itemBuilder: (context, index) {
+            return TransanctionCard(data: data);
+          },
+        );
+      },
+    );
   }
 }
 
 class TransanctionCard extends StatelessWidget {
-  TransanctionCard({
-    super.key,
-    
-  });
+  TransanctionCard({super.key,required this.data});
+  final dynamic data;
   var appIcons = AppIcons();
 
   @override
@@ -67,38 +75,36 @@ class TransanctionCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
           boxShadow: [
             BoxShadow(
-              offset:Offset(0,10),
+              offset: Offset(0, 10),
               color: Colors.grey.withOpacity(0.09),
               blurRadius: 10.0,
               spreadRadius: 4.0,
-              
-            
-            )
-          ]
+            ),
+          ],
         ),
         child: ListTile(
           minVerticalPadding: 10,
-          contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
           leading: Container(
             width: 70,
-            height:100,
+            height: 100,
             child: Container(
               width: 30,
               height: 30,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
                 color: Colors.green.withOpacity(0.2),
-            
               ),
-              child: Center(child: FaIcon(appIcons.getExpenseCategoryIcons('Rent'))),
+              child: Center(
+                child: FaIcon(appIcons.getExpenseCategoryIcons('Rent')),
+              ),
             ),
           ),
           title: Row(
             children: [
               Expanded(child: Text('Card Rent Oct 2023')),
-              Text('₹ 8000',style: TextStyle(color: Colors.green),)
+              Text('₹ 8000', style: TextStyle(color: Colors.green)),
             ],
-        
           ),
           subtitle: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -106,14 +112,18 @@ class TransanctionCard extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  Text("Balance",style: TextStyle(color: Colors.grey,fontSize: 13)),
+                  Text(
+                    "Balance",
+                    style: TextStyle(color: Colors.grey, fontSize: 13),
+                  ),
                   Spacer(),
-                  Text('₹ 525', style: TextStyle(color: Colors.grey,fontSize: 13),),
-        
-        
+                  Text(
+                    '₹ 525',
+                    style: TextStyle(color: Colors.grey, fontSize: 13),
+                  ),
                 ],
               ),
-              Text('25 oct 4:51 PM',style: TextStyle(color: Colors.grey),)
+              Text('25 oct 4:51 PM', style: TextStyle(color: Colors.grey)),
             ],
           ),
         ),
