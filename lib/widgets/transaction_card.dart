088ee4ba832fef84d3bc44f50719c030324f93e9
shishupaltech <veerutech75@gmail.com>
2025-06-04 13:27:24 +0530

    transanction_card is extracted in Transanctionrecentlist and tranctionscard or add collection of firebase to show dynamically

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -7,7 +7,7 @@ class TransactionCard extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    var appIcons = AppIcons();
+    
     return Padding(
       padding:const EdgeInsets.all(15),
       child: Column(
@@ -23,75 +23,100 @@ class TransactionCard extends StatelessWidget {
               )
             ],
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: 2,
-            physics: NeverScrollableScrollPhysics(),
-
-            itemBuilder: (context, index){
-            
-            return Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      offset:Offset(0,10),
-                      color: Colors.grey.withOpacity(0.09),
-                      blurRadius: 10.0,
-                      spreadRadius: 4.0,
-                      
-                    
-                    )
-                  ]
-                ),
-                child: ListTile(
-                  minVerticalPadding: 10,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
-                  leading: Container(
-                    width: 70,
-                    height:100,
-                    child: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.green.withOpacity(0.2),
-                    
-                      ),
-                      child: Center(child: FaIcon(appIcons.getExpenseCategoryIcons('Rent'))),
-                    ),
-                  ),
-                  title: Row(
-                    children: [
-                      Expanded(child: Text('Card Rent Oct 2023')),
-                      Text('₹ 8000',style: TextStyle(color: Colors.green),)
-                    ],
-                
-                  ),
-                  subtitle: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text("Balance",style: TextStyle(color: Colors.grey,fontSize: 13)),
-                          Spacer(),
-                          Text('₹ 525', style: TextStyle(color: Colors.grey,fontSize: 13),),
-                
-                
-                        ],
-                      ),
-                      Text('25 oct 4:51 PM',style: TextStyle(color: Colors.grey),)
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }),
+          RecentTransanctionList(),
         ],
+      ),
+    );
+  }
+}
+
+class RecentTransanctionList extends StatelessWidget {
+  const RecentTransanctionList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: 2,
+      physics: NeverScrollableScrollPhysics(),
+    
+      itemBuilder: (context, index){
+      
+      return TransanctionCard();
+    });
+  }
+}
+
+class TransanctionCard extends StatelessWidget {
+  TransanctionCard({
+    super.key,
+    
+  });
+  var appIcons = AppIcons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              offset:Offset(0,10),
+              color: Colors.grey.withOpacity(0.09),
+              blurRadius: 10.0,
+              spreadRadius: 4.0,
+              
+            
+            )
+          ]
+        ),
+        child: ListTile(
+          minVerticalPadding: 10,
+          contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+          leading: Container(
+            width: 70,
+            height:100,
+            child: Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.green.withOpacity(0.2),
+            
+              ),
+              child: Center(child: FaIcon(appIcons.getExpenseCategoryIcons('Rent'))),
+            ),
+          ),
+          title: Row(
+            children: [
+              Expanded(child: Text('Card Rent Oct 2023')),
+              Text('₹ 8000',style: TextStyle(color: Colors.green),)
+            ],
+        
+          ),
+          subtitle: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("Balance",style: TextStyle(color: Colors.grey,fontSize: 13)),
+                  Spacer(),
+                  Text('₹ 525', style: TextStyle(color: Colors.grey,fontSize: 13),),
+        
+        
+                ],
+              ),
+              Text('25 oct 4:51 PM',style: TextStyle(color: Colors.grey),)
+            ],
+          ),
+        ),
       ),
     );
   }
